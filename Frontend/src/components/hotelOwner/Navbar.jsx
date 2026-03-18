fee3680dcb5b7b5211333645d87dfeb576c6f223
Pushpendra Purve <pushpendrapurve221134@acropolis.in>
2026-03-18 17:49:26 +0530

    adding new logo to navbar and foter

--- Frontend/src/components/hotelOwner/Navbar.jsx
+++ Frontend/src/components/hotelOwner/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white transition-all duration-300">
       <Link to="/">
-        <img src={assets.logo} alt="logo" className="h-9 invert opacity-80" />
+        <img src={assets.logo2} alt="logo" className="h-15 invert opacity-80" />
       </Link>
 
       <button
